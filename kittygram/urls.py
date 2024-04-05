310311c690bab9b5e95de1fb07674088a628ce3f
Андрей Бровкин <a.brovkin4@yandex.ru>
2024-04-05 23:39:20 +0300

    Dif_variants

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,30 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
-from cats.views import cat_list
+# from cats.views import cat_list
+# from cats.views import APICat
+# from cats.views import CatList, CatDetail
+from cats.views import CatViewSet
 
-urlpatterns = [
+# Функции
+'''urlpatterns = [
    path('cats/', cat_list),
+]'''
+
+# Низкоуровневые view-классы
+'''urlpatterns = [
+   path('cats/', APICat.as_view()),
+]'''
+
+# Высокоуровневые классы
+'''urlpatterns = [
+   path('cats/', CatList.as_view()),
+   path('cats/<int:pk>/', CatDetail.as_view()),
+]'''
+
+# ViewSets
+router = SimpleRouter()
+router.register('cats', CatViewSet)
+urlpatterns = [
+   path('', include(router.urls)),
 ]
-
-
